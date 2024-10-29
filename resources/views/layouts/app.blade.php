06de8dea7e950a4050663605d61d860ac5cc8038
pivnati <natanmuleta911@gmail.com>
2024-10-28 19:38:20 -0700

    add the javascript code in the app.blade.php fiel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,21 +18,21 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500"> 
     <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-    <link rel="stylesheet" href=" {{asset('css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href=" {{asset('css/magnific-popup.css')}}">
-    <link rel="stylesheet" href=" {{asset('css/jquery-ui.css')}}">
-    <link rel="stylesheet" href="  {{asset('css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href=" {{asset('css/owl.theme.default.min.css')}}">
-    <link rel="stylesheet" href=" {{asset('css/bootstrap-datepicker.css')}}">
-    <link rel="stylesheet" href=" {{asset('css/mediaelementplayer.css')}}">
-    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
-    <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
-    <link rel="stylesheet" href=" {{asset('css/fl-bigmug-line.css')}}">
+    <link rel="stylesheet" href=" {{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href=" {{asset('assets/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href=" {{asset('assets/css/jquery-ui.css')}}">
+    <link rel="stylesheet" href="  {{asset('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href=" {{asset('assets/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href=" {{asset('assets/css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href=" {{asset('assets/css/mediaelementplayer.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/fonts/flaticon/font/flaticon.css')}}">
+    <link rel="stylesheet" href=" {{asset('assets/css/fl-bigmug-line.css')}}">
     
   
-    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href=" {{asset('assets/ css/aos.css')}}">
 
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
@@ -96,5 +96,20 @@
             @yield('content')
         </main>
     </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/mediaelement-and-player.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
 </body>
 </html>
